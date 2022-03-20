36ef97a8925842e2e0452c5e447f80e90bd7ff10
Ouail Zahir <ozahir@e2r10p2.1337.ma>
2022-03-20 22:43:11 +0100

    so_lomg

---
+++ mandatory/moves/p_moves.c
@@ -0,0 +1,143 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozahir <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 19:52:51 by ozahir            #+#    #+#             */
+/*   Updated: 2022/03/16 21:00:28 by ozahir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+
+void	move_up(t_mlx *mlx)
+{
+	int		i;
+	int		j;
+	char	**m;
+
+	m = mlx->map->map;
+	i = 0;
+	while (m[i])
+	{
+		j = 0;
+		while (m[i][j])
+		{
+			if (m[i][j] == 'P'
+					&& (m[i + 1][j] == '0'
+						|| m[i + 1][j] == 'C' || m[i + 1][j] == 'Q'))
+			{
+				m[i][j] = '0';
+				m[i + 1][j] = 'P';
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	move_down(t_mlx *mlx)
+{
+	int		i;
+	int		j;
+	char	**m;
+
+	m = mlx->map->map;
+	i = 0;
+	while (m[i])
+	{
+		j = 0;
+		while (m[i][j])
+		{
+			if (m[i][j] == 'P'
+					&& (m[i - 1][j] == '0'
+						|| m[i - 1][j] == 'C' || m[i - 1][j] == 'Q'))
+			{
+				m[i][j] = '0';
+				m[i - 1][j] = 'P';
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	move_left(t_mlx *mlx)
+{
+	int		i;
+	int		j;
+	char	**m;
+
+	m = mlx->map->map;
+	i = 0;
+	while (m[i])
+	{
+		j = 0;
+		while (m[i][j])
+		{
+			if (m[i][j] == 'P'
+					&& (m[i][j - 1] == '0'
+						|| m[i][j - 1] == 'C' || m[i][j - 1] == 'Q'))
+			{
+				m[i][j] = '0';
+				m[i][j - 1] = 'P';
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	move_right(t_mlx *mlx)
+{
+	int		i;
+	int		j;
+	char	**m;
+
+	m = mlx->map->map;
+	i = 0;
+	while (m[i])
+	{
+		j = 0;
+		while (m[i][j])
+		{
+			if (m[i][j] == 'P'
+					&& (m[i][j + 1] == '0' || m[i][j + 1] == 'C'
+						|| m[i][j + 1] == 'Q'))
+			{
+				m[i][j] = '0';
+				m[i][j + 1] = 'P';
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	change_prefix(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'E')
+			{
+				map[i][j] = 'Q';
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
